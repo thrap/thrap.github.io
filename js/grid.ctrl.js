d1f82d13fd1472d3f51423123acf3e2527cb7680
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-09 18:46:18 +0100

    First commit

---
+++ js/grid.ctrl.js
@@ -0,0 +1,11 @@
+function TicTacToeCtrl($scope) {
+    var SIZE = 6;
+    $scope.board = [];
+    for(var i = 0; i<SIZE; i++) {
+        var row = [];
+        for (var j=0; j<SIZE; j++) {
+            row.push({x:i, y:j})
+        }
+        $scope.board.push(row);
+    }
+}
